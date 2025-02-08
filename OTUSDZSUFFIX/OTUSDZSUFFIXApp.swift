4f8c50a831901b43dcbfbc6c6174c12472c3d703
3Vvv <333vovk@mail.ru>
2025-02-08 09:37:35 +0300

    Initial Commit

---
+++ OTUSDZSUFFIX/OTUSDZSUFFIXApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTUSDZSUFFIXApp.swift
+//  OTUSDZSUFFIX
+//
+//  Created by Вячеслав Вовк on 08.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct OTUSDZSUFFIXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
